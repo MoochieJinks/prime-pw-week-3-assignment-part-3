b9691b176129f101412f08a29d25506a5ecbd453
Harrison Browning <moochiejinks@harrisons-mbp.lan>
2022-03-20 13:53:26 -0500

    Added new lines to end of file

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -43,3 +43,7 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
+
+
+
